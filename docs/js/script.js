0b62ea0f20a9a29e2f71b4fd36e71de28193c831
Marketing2 <Marketing2@Marketing2s-iMac.local>
2018-03-01 13:20:14 -0500

    Threds Board import from codepen

--- docs/js/script.js
+++ docs/js/script.js
@@ -3,13 +3,54 @@ $(document).ready(function(){
 
 	// Build the page here.
 
-	var title = 'Threds Board'
-	var hw = 'Hello World'
-	
-	$('title').append(title)
 
-	$('body').append('<h1 class="hw"></h1>');
+	var sites = ["baylinerapparel", "capriottisgear", "capriottisgear", "copperfoodgear", "corppromoitems", "hatterasyachtsgear", "malibuboatsgear", "marquislarsoncollection", "mercuryproteamgear", "meridianyachtsgear", "nautiquegear", "salsaritasgear", "scapparel", "searaycollection", "shopsugarlands", "whalerapparel", "notarealsitelmao"];
 
-	$('.hw').text(hw);
+	var checkStatus = function(company){
+		var favid = "img." + company;
+		var aid = "a." + company;
+		$(aid).css({
+		});
+
+		$(favid).on('load', function() {
+			$(aid).css({
+
+			});
+		});
+	};
+
+	for (site in sites){
+		var website = "https://www." + sites[site] + ".com";
+		var img = '<img class="' + sites[site] + '" style="display: none;" src="' + website + '/favicon.ico">';
+		var li = '<li> <a class="' + sites[site] + '" href="' + website + '">' + img + sites[site] + '</a></li>';
+
+		$("#ulist").append(li);
+
+		checkStatus(sites[site])
+	};
+
+//Circle Graph
+
+$('#percent').on('change', function(){
+  var val = parseInt($(this).val());
+  var $circle = $('#svg #bar');
+  
+  if (isNaN(val)) {
+   val = 100; 
+  }
+  else{
+    var r = $circle.attr('r');
+    var c = Math.PI*(r*2);
+   
+    if (val < 0) { val = 0;}
+    if (val > 100) { val = 100;}
+    
+    var pct = ((100-val)/100)*c;
+    
+    $circle.css({ strokeDashoffset: pct});
+    
+    $('#cont').attr('data-pct',val);
+  }
+});
 
 });
