10f623bc2c77e350e9d5af8db0f9eab1086f62fe
thredsweb <36961395+thredsweb@users.noreply.github.com>
2018-03-02 10:02:18 -0500

    added reload button

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,14 +1,6 @@
-$(document).ready(function(){
+$(document).ready( function() {
 
 	var sites = ["baylinerapparel", "capriottisgear", "capriottisgear", "copperfoodgear", "corppromoitems", "hatterasyachtsgear", "malibuboatsgear", "marquislarsoncollection", "mercuryproteamgear", "meridianyachtsgear", "nautiquegear", "salsaritasgear", "scapparel", "searaycollection", "shopsugarlands", "whalerapparel", "notarealsitelmao"];
-
-	function stallForTime() {
-		return new Promise(resolve => {
-			setTimeout(() => {
-				resolve('resolved');
-			}, 500);
-		});
-	}
 
 	function checkStatus (company) {
 		var favid = "img." + company;
@@ -48,39 +40,36 @@ $(document).ready(function(){
 		}
 	}
 
-	for (site in sites) {
-		var website = "https://www." + sites[site] + ".com";
-		var img = '<img class="' + sites[site] + '" style="display: none;" src="' + website + '/favicon.ico">';
-		var li = '<li> <a class="' + sites[site] + '" href="' + website + '">' + img + sites[site] + '</a></li>';
-
-		$("#status_ul").append(li);
-		checkStatus(sites[site])
+	function resetStatus() {
+			$('#status_ul').children().remove();
 	}
 
+	function stallForTime() {
+		return new Promise(resolve => {
+			setTimeout(() => {
+				resolve('resolved');
+			}, 500);
+		});
+	}
+	function statusBuild() {
+		for (site in sites) {
+			var website = "https://www." + sites[site] + ".com";
+			var img = '<img class="' + sites[site] + '" style="display: none;" src="' + website + '/favicon.ico">';
+			var li = '<li> <a class="' + sites[site] + '" href="' + website + '">' + img + sites[site] + '</a></li>';
+	
+			$("#status_ul").append(li);
+			checkStatus(sites[site])
+		}
+	}
+
+	$('#status_widget').append('<button id="status_reload">Reload</button>');
 	$('#status_text').append('Checking Status...');
+	statusBuild();
 	countEm();
 
-	//Circle Graph
-	// $('#percent').on('change', function(){
-	// 	var val = parseInt($(this).val());
-
-	// 	var $circle = $('#svg #bar');
-
-	// 	if (isNaN(val)) {
-	// 		val = 100; 
-
-	// 	} else {
-	// 		var r = $circle.attr('r');
-	// 		var c = Math.PI*(r*2);
-
-	// 		if (val < 0) { val = 0;}
-	// 		if (val > 100) { val = 100;}
-
-	// 		var pct = ((100-($('.status_red').length))/100)*c;
-
-	// 		$circle.css({ strokeDashoffset: pct});
-
-	// 		$('#cont').attr('data-pct',val);
-	// 	}
-	// });
+	$('#status_reload').on( 'click', () => {
+		resetStatus();
+		statusBuild();
+		countEm();
+	});
 });
