716c71eab8328bae2b53ec627912013d754c6ba1
Marketing2 <Marketing2@Marketing2s-iMac.local>
2018-03-02 08:52:53 -0500

    Status Widget Circle Progress

--- docs/js/script.js
+++ docs/js/script.js
@@ -23,11 +23,28 @@ $(document).ready(function(){
 
 	async function countEm(){
 		const waiting = await stallForTime();
-		$('#status_text').empty().append($('.status_green').length + ' / ' + sites.length + ' sites are live');
+		var $circle = $('#svg #bar');
+		var $green = $('.status_green');
+		var $red = $('.status_red');
+		var status_val = ($green.length / sites.length) * 100;
+
+		$('#status_text').empty().append($green.length + ' / ' + sites.length + ' sites are live');
+
 		if (($('.status_green').length / sites.length) === 1) {
 			$('#status_text').append('<br/>All Systems Functional');
 		} else {
 			$('#status_text').append('<br/>Partial Problems');
+		}
+
+		if (isNaN(status_val)) {
+			status_val = 100; 
+		} else {
+			var r = $circle.attr('r');
+			var c = Math.PI*(r*2);
+			if (status_val < 0) { status_val = 0;}
+			if (status_val > 100) { status_val = 100;}
+			$circle.css({ strokeDashoffset: -(status_val / 100 * c)});
+			$('#cont').attr('data-pct', Math.floor(status_val));
 		}
 	}
 
@@ -43,13 +60,27 @@ $(document).ready(function(){
 	$('#status_text').append('Checking Status...');
 	countEm();
 
-	// $('#status_text').append($('.status_green').length + ' / ' + sites.length + ' sites are live');
-	// if (($('.status_green').length / sites.length) === 1) {
-	// 	$('#status_text').append('<br/>All Systems Functional');
-	// } else {
-	// 	$('#status_text').append('<br/>Partial Problems');
-	// }
-
 	//Circle Graph
+	// $('#percent').on('change', function(){
+	// 	var val = parseInt($(this).val());
 
+	// 	var $circle = $('#svg #bar');
+
+	// 	if (isNaN(val)) {
+	// 		val = 100; 
+
+	// 	} else {
+	// 		var r = $circle.attr('r');
+	// 		var c = Math.PI*(r*2);
+
+	// 		if (val < 0) { val = 0;}
+	// 		if (val > 100) { val = 100;}
+
+	// 		var pct = ((100-($('.status_red').length))/100)*c;
+
+	// 		$circle.css({ strokeDashoffset: pct});
+
+	// 		$('#cont').attr('data-pct',val);
+	// 	}
+	// });
 });
