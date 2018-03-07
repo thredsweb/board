62c7040e86955bb10bb42b640cc08770464b753b
Marketing2 <Marketing2@Marketing2s-iMac.local>
2018-03-07 08:14:26 -0500

    tweaking ng

---
+++ docs/js/core.js
@@ -0,0 +1,2 @@
+export var sites = ["baylinerapparel", "capriottisgear", "copperfoodgear", "corppromoitems", "hatterasyachtsgear", "malibuboatsgear", "marquislarsoncollection", "mercuryproteamgear", "meridianyachtsgear", "nautiquegear", "salsaritasgear", "scapparel", "searaycollection", "shopsugarlands", "whalerapparel"];
+export var pApp = angular.module('pApp', []);
